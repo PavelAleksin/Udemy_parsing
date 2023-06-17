84e0fa2b61cbca525d143eacb55d5963db3c123c
Pavel <130147398+PavelAleksin@users.noreply.github.com>
2023-06-17 17:24:22 +0300

    1

--- Les6.py
+++ Les6.py
@@ -7,3 +7,21 @@ r.post("https://www.httpbin.org/post").json()
 HEAD
 OPTIONS
 
+import requests as r
+resp = r.post("https://www.httpbin.org/post",data={'key1':'value1'})
+resp.json()
+resp = r.post("https://www.httpbin.org/post",data={
+                                                'key1':'value1',
+                                                'key2':'avalue with'
+                                                })
+resp.request.body
+resp = r.post("https://www.httpbin.org/post",data='some text')
+resp.request.body
+resp = r.post("https://www.httpbin.org/post",json={
+                                                'key1':'value1',
+                                                'key2':'avalue with'
+                                                })
+
+resp.json()
+resp.request.body
+JSON -> JavaScript Object Notation
