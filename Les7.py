fe40751ed16a6e2333b8be703390393e1a7b3fa5
Pavel <130147398+PavelAleksin@users.noreply.github.com>
2023-06-17 21:35:01 +0300

    save

--- Les7.py
+++ Les7.py
@@ -3,41 +3,65 @@ from bs4 import BeautifulSoup
 resp = r.get('https://books.toscrape.com/')
 resp.content
 soup = BeautifulSoup(resp.content) #lxml
-type(soup)
-soup.title
-print(soup.prettify())
-soup.name
-------------------------------------------
-soup.h1
-first_div = soup.div
-first_div.find
-soup.div
-soup.ul
-print(soup.ul.prettify())
-list(soup.ul.children)
-from bs4.element import NavigableString
-list(filter(lambda x: type(x) != NavigableString,soup.ul.children))
-def no_nav_strings(iterable):
-    return list(filter(lambda x: type(x) != NavigableString,iterable))
-no_nav_strings(soup.ul.children)
-list(soup.ul.descendants)
-desc = no_nav_strings(soup.ul.descendants)
-desc[0]
-desc[0].parent
----------------------------------------------
-soup.ul.li
-soup.ul.li\
-        .next_sibling\
-            .next_sibling\
-                .previous_sibling
-soup.a
-soup.a.get_text()
-soup.a.string
-soup.a.text
-soup.ul.get_text()
-soup.ul.string
-soup.ul.text
-print(soup.a.text,'of type',type(soup.a.text))
-print(soup.a.get_text(),'of type',type(soup.a.get_text()))
-print(soup.a.string,'of type',type(soup.a.string))
-soup.ul.get_text(separator=',',strip=True)
+# type(soup)
+# soup.title
+# print(soup.prettify())
+# soup.name
+#------------------------------------------
+# soup.h1
+# first_div = soup.div
+# first_div.find
+# soup.div
+# soup.ul
+# print(soup.ul.prettify())
+# list(soup.ul.children)
+# from bs4.element import NavigableString
+# list(filter(lambda x: type(x) != NavigableString,soup.ul.children))
+# def no_nav_strings(iterable):
+#     return list(filter(lambda x: type(x) != NavigableString,iterable))
+# no_nav_strings(soup.ul.children)
+# list(soup.ul.descendants)
+# desc = no_nav_strings(soup.ul.descendants)
+# desc[0]
+# desc[0].parent
+#---------------------------------------------
+# soup.ul.li
+# soup.ul.li\
+#         .next_sibling\
+#             .next_sibling\
+#                 .previous_sibling
+# soup.a
+# soup.a.get_text()
+# soup.a.string
+# soup.a.text
+# soup.ul.get_text()
+# soup.ul.string
+# soup.ul.text
+# print(soup.a.text,'of type',type(soup.a.text))
+# print(soup.a.get_text(),'of type',type(soup.a.get_text()))
+# print(soup.a.string,'of type',type(soup.a.string))
+#soup.ul.get_text(separator=',',strip=True)
+# soup.stripped_strings
+# all_string = list(soup.stripped_strings)
+# len(all_string)
+# len(list(soup.strings))
+# list(soup.strings)[:5]
+#--------------------------------------------find--find_all---------------------------
+# soup.find_all()
+# len(soup.find_all())
+# soup.find_all('a')
+# len(soup.find_all('a'))
+# len(soup.find_all(['a','p']))
+#-------------------------------------------------
+# tag_name: p
+# attr:class=price_color
+# price_tags = soup.find_all('p',attrs={'class':'price_color'})
+# [price.get_text() for price in price_tags]
+# price_tags = soup.find_all('p',class_='price_color')
+# [price.get_text() for price in price_tags]
+# add_buttons = soup.find_all('button',attrs={'data-loading-text':'Adding...'})
+# len(add_buttons)
+# add_buttons = soup.find_all('button',
+#                             attrs={'data-loading-text':lambda x: 'add' in x.lower() or 'remove' in x.lower()})
+# len(add_buttons)
+
---
+++ Task_1.py
@@ -0,0 +1,41 @@
+import requests as r
+from bs4 import BeautifulSoup
+import re
+resp = r.get('https://books.toscrape.com/')
+soup = BeautifulSoup(resp.content) 
+print(f'Статус запроса:{resp.status_code}')
+len(soup.find_all())
+
+#1 вариант очистка от символа ваалюты и перевод во Float
+def clean_price(price):
+    return float(''.join([char for char in price if char.isdigit() or char == '.']))
+
+#2 вариант(cost) очистка от символа ваалюты и перевод во Float
+def clean_price2(price):
+    return float(re.sub('[^0-9.]','',price))
+
+def convert_rating(ratings):
+    rating={
+        'One':1,
+        'Two':2,
+        'Three':3,
+        'Four':4,
+        'Five':5
+    }
+    return rating[ratings]
+
+
+def extract_book(book):
+    title = book.find('h3').find('a')['title']
+    price = book.find('p',attrs={'class':'price_color'}).text #replace('£','')
+    stats = book.find('p',attrs={'class':f'star-rating'})['class'][-1]
+    return {
+            'title':title,
+            'price':clean_price(price),
+            'stats':convert_rating(stats)
+    }
+
+books = soup.find_all('article',attrs={'class':'product_pod'})
+books_data = [extract_book(books) for books in books]
+
+books_data
