74003411269a0c3846c9119f875463cc9be1f3bf
PavelAL <PavelAleksin90@gmail.com>
2023-06-26 11:56:02 +0300

    1

--- Les_Project_1(Finance).py
+++ Les_Project_1(Finance).py
@@ -1,12 +1,9 @@
 import requests as r
 from bs4 import  BeautifulSoup
-def get_price_information(ticket,exchange):
-    proxy={'schema' : 'http', 'address' :'167.172.96.117:38295'}
-    url = (f'https://www.google.com/finance/quote/{ticket}:{exchange}')
-    resp = r.get(url,proxies=proxy)
-    soup = BeautifulSoup(resp.content,'html.parser')
-
-    price_div = soup.find('div',attrs = {'data-last-price': True})
-    return price_div
-
-print(get_price_information('MSFT','NASDAQ'))
+# def get_price_information(ticket,exchange):
+proxy={'schema' : 'http', 'address' :'167.172.96.117:38295'}
+url = (f'https://www.google.com/finance/quote/MSFT:NASDAQ')
+resp = r.get(url,proxies=proxy)
+soup = BeautifulSoup(resp.content,'html.parser')
+price_div = soup.find('div',attrs = {'data-last-price': True})
+print(soup)
